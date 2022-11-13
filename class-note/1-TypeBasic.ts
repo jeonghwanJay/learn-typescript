6b13fbcef77e64c9e2ff61dd469742123cf0aa60
jeonghwanJay <nawhgnoejij@naver.com>
2022-11-13 18:47:38 +0900

    edit : 1-TypeBasic.ts

--- class-note/1-TypeBasic.ts
+++ class-note/1-TypeBasic.ts
@@ -9,3 +9,19 @@ const arr: Array<number> = [10, 20]
 const arr1: number[] = [10, 20]
 const array: Array<string> = ['Jay', 'John', 'smith']
 const array1: string[] = ['Jay', 'John', 'smith']
+
+// TS 튜플
+const profile: [string, number] = ["Jay", 10]
+const profile1: [number, string, string] = [100, "John", "developer"]
+
+// TS 객체
+const obj: object = {
+    name: "Jay",
+    age: 10,
+    job: 'developer'
+}
+const obj1: {name:string, age:number, job:string} = {
+    name: "Jay",
+    age: 10,
+    job: 'developer'
+}
