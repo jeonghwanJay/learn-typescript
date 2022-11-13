11ff9bfcd1695850d256afd1a77ab1ed5892e04c
jeonghwanJay <nawhgnoejij@naver.com>
2022-11-13 18:30:36 +0900

    add : 1-TypeBasic.ts

---
+++ class-note/1-TypeBasic.ts
@@ -0,0 +1,11 @@
+// TS 문자열
+const str:string = 'hello';
+
+// TS 숫자
+const num: number = 100;
+
+// TS 배열
+const arr: Array<number> = [10, 20]
+const arr1: number[] = [10, 20]
+const array: Array<string> = ['Jay', 'John', 'smith']
+const array1: string[] = ['Jay', 'John', 'smith']
