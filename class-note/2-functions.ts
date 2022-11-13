4a16168dc8811bcad3a4fed9f4c1fa76edc9de86
jeonghwanJay <nawhgnoejij@naver.com>
2022-11-13 18:55:51 +0900

    add : 2-functions.ts

---
+++ class-note/2-functions.ts
@@ -0,0 +1,14 @@
+// 함수 파라미터에 타입을 정의하는 방식
+function add(a: number, b: number) {
+    return a + b;
+}
+
+// 함수 반환값에 타입을 정의하는 방식
+function add1(a,b): number {
+    return a + b;
+}
+
+// 함수에 타입을 정의하는 방식
+function add2(a: number, b: number): number {
+    return a + b;
+}
